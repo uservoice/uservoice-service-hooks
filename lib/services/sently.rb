2342f33738f22aba8031aaec85525300190f57f8
Raimo Tuisku <raimo.tuisku@gmail.com>
2013-12-04 17:39:15 -0800

    Reformat

--- lib/services/sently.rb
+++ lib/services/sently.rb
@@ -8,13 +8,13 @@ class Services::Sently < Services::Base
 
   def perform
     uri = URI.parse("https://sent.ly/uservoice/updatehandler")
-	http = Net::HTTP.new(uri.host, uri.port)
-	http.use_ssl = true
-	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	request = Net::HTTP::Post.new(uri.path)
-	request["Content-Type"] = "application/x-www-form-urlencoded"
-	request.body = "data=" + message
-	response = http.request(request)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(uri.path)
+    request["Content-Type"] = "application/x-www-form-urlencoded"
+    request.body = "data=" + message
+    response = http.request(request)
     return response.is_a?(Net::HTTPSuccess)
   end
 
